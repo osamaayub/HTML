30ae06904d12a9e47bae401a088e8c2c43a1bb08
Osama ayub <77015981+osamaayub@users.noreply.github.com>
2021-11-29 03:37:59 -0800

    Add files via upload

---
+++ style.js
@@ -0,0 +1,18 @@
+console.log("HelloWOrld");
+alert("HelloWOrld");
+
+var a=5;
+var b=4;
+if(a<b)
+{
+	alert("a is less than b");
+}
+else
+{
+	alert("a is greater than b");
+}
+
+for(i=0;i<10;i++)
+{
+	alert("value of i is:"+i);
+}
